c8dc6ed3246c6ab36480544f3d879eb2a3765864
Todd Traylor <toddt67878@yahoo.com>
2020-04-05 16:55:14 -0500

    Chapter 8 Commit

---
+++ ch8_Fibonacci_Sequence.py
@@ -0,0 +1,6 @@
+define main():
+         print("This program computes a Fibonacci number.")
+         n = int(input(
+             "Please enter what term in the Fibonnaci sequence you would like to know."))
+         for i in range(n):
+             (i + (i - 1))
