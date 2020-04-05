37abcdabfde8525550c75f6324d20af0557d7e1c
Todd Traylor <toddt67878@yahoo.com>
2020-04-05 16:40:29 -0500

    Chapeter 2 commit

---
+++ ch2_convert.py
@@ -0,0 +1,8 @@
+def main():
+    print("This program coverts a temperature in Celsius to a temperature in Fahrenheit.")
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9/5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+
+
+main()
